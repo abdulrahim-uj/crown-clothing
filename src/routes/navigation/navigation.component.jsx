6c64a7939caedf21a4bd3c60399d105cc415a57d
AbdulRahim Kattirithodi <abdulrahim.uj@gmail.com>
2022-07-05 16:49:52 +0530

    added cart items, dropdown list and cart context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,12 +2,17 @@ import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrownLogo } from '../../assets/navigation/crown.svg';
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropDown from "../../components/cart-dropdown/cart-dropdown.component";
 import './navigation.styles.scss';
 
 const Navigation = () => {
     // ACCESSING THE CURRENT USER DATA FROM USERCONTEXT
     const { currentUser } = useContext(UserContext);
+
+    const { isCartOpen } = useContext(CartContext);
 
     const signOutHandler = async () => {
         await signOutUser();
@@ -32,7 +37,11 @@ const Navigation = () => {
                             <Link className="nav-link" to="/auth">Sign In</Link>
                         )
                     }
+                    <CartIcon />
                 </div>
+                {
+                    isCartOpen && <CartDropDown />
+                }
             </div>
             <Outlet />
         </Fragment>
