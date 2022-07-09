c30602a687c2397251d382ff7bacebeebfbf503d
AbdulRahim Kattirithodi <abdulrahim.uj@gmail.com>
2022-07-09 10:58:25 +0530

    Added nested Routes

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,22 @@
+import { Fragment, useContext } from "react";
+import { CategoriesContext } from "../../contexts/categories.context";
+// import ProductCard from "../../components/product-card/product-card.component";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+import './categories-preview.styles.scss';
+
+const CategoriesPreview = () => {
+    const {categoriesMap} = useContext(CategoriesContext);
+    return (
+        <Fragment>
+            {
+                Object.keys(categoriesMap).map(title => {
+                    const products = categoriesMap[title];
+                    console.log('PRODUCTS ARRAY', products);
+                    return <CategoryPreview key={title} titleValue={title} productsArray={products} />
+                })
+            }
+        </Fragment>
+    );
+};
+
+export default CategoriesPreview;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,21 +1,12 @@
-import { useContext } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
-// import ProductCard from "../../components/product-card/product-card.component";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
+import { Routes, Route } from 'react-router-dom';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
 import './shop.styles.scss';
 
 const Shop = () => {
-    const {categoriesMap} = useContext(CategoriesContext);
     return (
-        <div className="shop-container">
-            {
-                Object.keys(categoriesMap).map(title => {
-                    const products = categoriesMap[title];
-                    console.log('PRODUCTS ARRAY', products);
-                    return <CategoryPreview key={title} titleValue={title} productsArray={products} />
-                })
-            }
-        </div>
+        <Routes>
+            <Route index={true} element={<CategoriesPreview />}  />
+        </Routes>
     );
 };
 
