96ad5dd709bd32cd2f90b3ae697541ce99891864
AbdulRahim Kattirithodi <abdulrahim.uj@gmail.com>
2022-07-09 16:57:09 +0530

    updated styled component to reusable Button Component

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,4 +1,4 @@
-import './button.styles.scss';
+import { BaseButton, GoogleSignInButton, InvertedButton } from './button.styles';
 
 /*
 3 different buttons
@@ -10,15 +10,23 @@ import './button.styles.scss';
 
 */
 // if we pass buttonType="inverted" it will take inverted css design
-const BUTTON_TYPE_CLASSES = {
+export const BUTTON_TYPE_CLASSES = {
+    base: 'base',
     google: 'google-sign-in',
     inverted: 'inverted',
-}
+};
+
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => ({
+    [BUTTON_TYPE_CLASSES.base]: BaseButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+}[buttonType]);
 
 // children will get button name eg: <button>Sign Up</button> so, children value is 'Sign Up'
 const Button = ({children, buttonType, ...otherProps}) => {
+    const CustomButton = getButton(buttonType);
     return (
-        <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`} {...otherProps}>{children}</button>
+        <CustomButton {...otherProps}>{children}</CustomButton>
     );
 };
 
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -5,7 +5,7 @@ import {
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 import './sign-in-form.styles.scss';
-import Button from "../button/button.component";
+import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
 
 // SET DEFAULT VALUES ON COMPONENT DID MOUNT TIME
 const defaultFormFields = {
@@ -91,7 +91,7 @@ const SignInForm = () => {
                         <Button buttonType="inverted" type={"submit"}>Sign Up</Button>    
                     */}
                     <Button type={"submit"}>Sign In</Button>
-                    <Button type={"button"} buttonType={"google"} onClick={signInWithGoogle}>Google Sign In</Button>
+                    <Button type={"button"} buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>Google Sign In</Button>
                 </div>
             </form>
         </div>
