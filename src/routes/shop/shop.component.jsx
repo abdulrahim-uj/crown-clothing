d8d9bead41e9e814e112c8a0c1c41e6d28122ff0
AbdulRahim Kattirithodi <abdulrahim.uj@gmail.com>
2022-08-13 17:59:32 +0530

    added redux-thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/category.action';
+// import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { 
+    /* setCategories, REMOVED USING REDUX_THUNK*/
+    fetchCategoriesThunkAsync
+} from '../../store/categories/category.action';
 import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
@@ -12,12 +15,7 @@ const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocuments();
-            dispatch(setCategories(categoriesArray));
-        };
-
-        getCategoriesMap();
+        dispatch(fetchCategoriesThunkAsync());
     }, [dispatch]);
 
     return (
--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,9 +1,43 @@
 import CATEGORIES_ACTION_TYPES from "./category.types";
 import { createActionForDispatch } from "../../utils/reducer/reducer.utils";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
+/* REMOVED USING REDUX_THUNK
 export const setCategories = (categoriesArray) => {
     return createActionForDispatch(
         CATEGORIES_ACTION_TYPES.SET_CATEGORIES,
         categoriesArray
     );
 };
+*/
+
+export const fetchCategoriesStart = () => {
+    return createActionForDispatch(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START
+    );
+};
+
+export const fetchCategoriesSuccess = (categoriesArray) => {
+    return createActionForDispatch(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+        categoriesArray
+    );
+};
+
+export const fetchCategoriesFailed = (error) => {
+    return createActionForDispatch(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+        error
+    );
+};
+
+export const fetchCategoriesThunkAsync = () => async(dispatchfunc) => {
+    dispatchfunc(fetchCategoriesStart());
+    
+    try {
+        const categoriesArray = await getCategoriesAndDocuments();
+        dispatchfunc(fetchCategoriesSuccess(categoriesArray));
+    } catch (error) {
+        dispatchfunc(fetchCategoriesFailed(error));
+    }
+}
